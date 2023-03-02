a13ed6546c1c2b4ac563b2c195a7b11b027c10eb
zliimt717 <xin.gu1707@gmail.com>
2023-03-02 16:59:59 +0100

    commit senario2

--- src/test/java/StepDefinition/Steps.java
+++ src/test/java/StepDefinition/Steps.java
@@ -4,12 +4,18 @@ package StepDefinition;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Steps {
-
+    WebDriver driver;
     @Given("^Open the Firefox and launch the application$")
     public void open_the_Firefox_and_launch_the_application() throws Throwable{
-        System.out.println("This step open the Firefox and launch the application.");
+        //System.out.println("This step open the Firefox and launch the application.");
+        System.setProperty("webdriver.gecko.driver",
+                "C:\\Users\\xin.gu\\SpringPractices\\cucumberwithSelenium\\src\\test\\resources\\geckodriver.exe");
+        driver=new FirefoxDriver();
+        driver.get("http://demo.guru99.com/v4");
     }
     @When("^Enter the Username and password$")
     public void enter_the_username_and_password () throws Throwable{
