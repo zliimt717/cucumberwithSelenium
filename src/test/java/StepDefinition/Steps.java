d8c423ce604cba489987f3b7cd7f1138b960746c
zliimt717 <xin.gu1707@gmail.com>
2023-03-03 10:38:15 +0100

    commit change the folder name and edit script

--- src/test/java/StepDefinition/Steps.java
+++ src/test/java/StepDefinition/Steps.java
@@ -4,6 +4,7 @@ package StepDefinition;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -13,17 +14,18 @@ public class Steps {
     public void open_the_Firefox_and_launch_the_application() throws Throwable{
         //System.out.println("This step open the Firefox and launch the application.");
         System.setProperty("webdriver.gecko.driver",
-                "C:\\Users\\xin.gu\\SpringPractices\\cucumberwithSelenium\\src\\test\\resources\\geckodriver.exe");
+                "C:\\Users\\xin.gu\\SpringPractices\\cucumberwithSelenium\\src\\test\\Drivers\\geckodriver.exe");
         driver=new FirefoxDriver();
-        driver.get("http://demo.guru99.com/v4");
+        driver.get("https://demo.guru99.com/v1/index.php");
     }
     @When("^Enter the Username and password$")
     public void enter_the_username_and_password () throws Throwable{
-        System.out.println("This step enter the Username and Password on the login page.");
+       driver.findElement(By.name("uid")).sendKeys("username12");
+       driver.findElement(By.name("password")).sendKeys("password12");
     }
     @Then("^Reset the credential$")
     public void Reset_the_credential() throws Throwable{
-        System.out.println("This step click on the Rest button.");
+        driver.findElement(By.name("btnReset")).click();
     }
 
 }
